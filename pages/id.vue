f35ba3232eeac42d65605f7b00513e88dde92fe7
Mehdi <nikanuser@gmail.com>
2025-04-19 20:14:00 +0330

    create login and id page

---
+++ pages/id.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="flex justify-center items-center w-full h-[95vh]">
+    <div class="flex flex-col justify-center items-center lg:w-3/12 md:w-5/12 sm:w-7/12 w-10/12 p-3 h-80 bg-[#0e1932] rounded-lg shadow-lg border-gray-700 border-[1px]">
+        <span>
+            <i class="fa-solid fa-headset text-5xl text-white"></i>
+        </span>
+        <span class="mt-4 mb-3 text-center text-sm text-gray-200">برای ورود به کارتابل خود ،لطفا شناسه خود را وارد کنید</span>
+        <input type="number" placeholder="شناسه را اینجا وارد کنید" class="relative block w-10/12 mt-4 disabled:cursor-not-allowed disabled:opacity-75 focus:outline-none border-0 form-input rounded-md placeholder-gray-400 dark:placeholder-gray-500 text-sm px-2.5 py-1.5 shadow-sm bg-white dark:bg-gray-900 text-gray-900 dark:text-white ring-1 ring-inset ring-gray-300 dark:ring-gray-700 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400" value="" style="background: none;">
+        <button value="" class="text-center p-1 text-sm w-5/12 mt-4 bg-green-600 hover:bg-green-500 cursor-pointer text-white rounded-lg" name="" id="">
+            <i class="fa-solid fa-right-to-bracket"></i>
+            اعتبار سنجی
+        </button>
+    </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="flex justify-center items-center w-full h-[95vh]">
+    <div class="flex flex-col justify-center items-center lg:w-3/12 md:w-5/12 sm:w-7/12 w-10/12 p-3 h-80 bg-[#0e1932] rounded-lg shadow-lg border-gray-700 border-[1px]">
+        <span>
+            <i class="fa-solid fa-right-to-bracket text-5xl text-white"></i>
+        </span>
+        <span class="mt-4 text-center text-base text-gray-100">مهدی عباسیان خوش آمدید</span>
+        <span class="mt-1 mb-3 text-center text-sm text-gray-300">لطفا نام کاربری و گذرواژه خود را وارد کنید</span>
+        <input type="text" placeholder="نام کاربری" class="relative block w-10/12 mt-4 disabled:cursor-not-allowed disabled:opacity-75 focus:outline-none border-0 form-input rounded-md placeholder-gray-400 dark:placeholder-gray-500 text-sm px-2.5 py-1.5 shadow-sm bg-white dark:bg-gray-900 text-gray-900 dark:text-white ring-1 ring-inset ring-gray-300 dark:ring-gray-700 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400" value="" style="background: none;">
+        <input type="password" placeholder="گذرواژه" class="relative block w-10/12 mt-4 disabled:cursor-not-allowed disabled:opacity-75 focus:outline-none border-0 form-input rounded-md placeholder-gray-400 dark:placeholder-gray-500 text-sm px-2.5 py-1.5 shadow-sm bg-white dark:bg-gray-900 text-gray-900 dark:text-white ring-1 ring-inset ring-gray-300 dark:ring-gray-700 focus:ring-2 focus:ring-primary-500 dark:focus:ring-primary-400" value="" style="background: none;">
+        <button value="" class="text-center p-1 text-sm w-5/12 mt-4 bg-green-600 hover:bg-green-500 cursor-pointer text-white rounded-lg" name="" id="">
+            <i class="fa-solid fa-door-open"></i>
+            ورود به کارتابل
+        </button>
+    </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
